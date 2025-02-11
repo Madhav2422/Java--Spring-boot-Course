ee70076e6151c5ed94f96edc08bcb6fb44b1807d
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-11 17:40:55 +0530

    Sum of the two numbers

---
+++ p2sum.java
@@ -0,0 +1,10 @@
+public class p2sum {
+    public static void main(String[] args) {
+        int num1=3;
+        int num2=4;
+
+        int result=num1+num2;
+
+        System.out.println(result);
+    }
+}
