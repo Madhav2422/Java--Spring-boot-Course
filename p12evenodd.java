650f77769e2e6d92fae2a0d0023fb1b9c8f7f325
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-12 18:28:07 +0530

    Even Odd

---
+++ p12evenodd.java
@@ -0,0 +1,16 @@
+class p12evenodd{
+    public static void main(String[] args) {
+        int n=10;
+
+        int result=0;
+
+        if(n%2==0){
+            result=10;
+           
+        }
+        else{
+            result=20;
+        }
+        System.out.println(result);
+    }
+}
