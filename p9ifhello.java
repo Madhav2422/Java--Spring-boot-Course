ea2c9080b42cc89b238deb1f440df4c88dc719cd
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-12 18:26:42 +0530

    If statement

---
+++ p9ifhello.java
@@ -0,0 +1,14 @@
+public class p9ifhello {
+    public static void main(String[] args) {
+        
+        int x=0;
+
+        if(x>8){
+            System.out.println("Hello");
+        }
+        else{
+            System.out.println("Bye");
+        }
+
+    }
+}
