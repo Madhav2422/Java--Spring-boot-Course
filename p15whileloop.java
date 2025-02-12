6c6e53fa21c6dd5bd5407b374edd4fdc53a47e8b
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-12 18:29:35 +0530

    while loop

---
+++ p15whileloop.java
@@ -0,0 +1,12 @@
+public class p15whileloop {
+    public static void main(String[] args) {
+        int i =1;
+
+        while(i<=4){
+            System.out.println("hii " + i);
+            i++;
+        }
+
+        System.out.println("bye");
+    }
+}
