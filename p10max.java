20bdf040597ce8cef169ce68781cf9b28c5d17af
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-12 18:27:13 +0530

    Max between two

---
+++ p10max.java
@@ -0,0 +1,17 @@
+public class p10max {
+    public static void main(String[] args) {
+        int x=10;
+
+        int y=1000;
+
+
+
+        if(x>y){
+            System.out.println("Max between two is  " + x);
+        }
+        else{
+            System.out.println("Max between two is " + y);   
+        }
+
+    }
+}
