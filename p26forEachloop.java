06728a112e8ab5b1723cde75a72906698700c5f8
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-13 17:51:16 +0530

    forEachLoop

---
+++ p26forEachloop.java
@@ -0,0 +1,10 @@
+public class p26forEachloop {
+    public static void main(String[] args) {
+        int n[]=new int[6];
+
+        for(int no:n){
+            System.out.println(no);
+        }
+
+    }
+}
