4c3c39b3124af0b9815305995ecd828b8ff1d1b1
Madhav Hirani <madhavhirani2422@gmail.com>
2025-02-14 10:56:02 +0530

    naming conventions

---
+++ p36namingconventions.java
@@ -0,0 +1,13 @@
+//Java follows the camel case naming
+
+//Camel casing
+
+//class and interface=Calc and Runable,CalcMyData
+//variable and method=marks,show(),showMyData()
+//constants=PIE,BRAND
+
+public class p36namingconventions {
+    public static void main(String[] args) {
+        
+    }
+}
